ac1445e6e396dcb3dc2549b4394504697d3b9155
The Large Hat Man <99277213+LargeHatMan@users.noreply.github.com>
2022-02-10 09:14:30 -0800

    Code For Mr.Yang

---
+++ assignment2.py
@@ -0,0 +1,5 @@
+#!python3
+
+print("Sample Output Task #2")
+print("This is my Second Command")
+print('it uses "two commands to desplay the out put')
---
+++ assignment5.py
@@ -0,0 +1,6 @@
+print("Ozymandias")
+print('by Percy Bysshe Shelley\n\nI met a traveller from an antique land,\nWho said—“Two vast and trunkless legs of stone\nStand in the desert. . . . Near them, on the sand,')
+print('Half sunk a shattered visage lies, whose frown,\nAnd wrinkled lip, and sneer of cold command,\nTell that its sculptor well those passions read')
+print('Which yet survive, stamped on these lifeless things,\nThe hand that mocked them, and the heart that fed;\nAnd on the pedestal, these words appear:')
+print('My name is Ozymandias, King of Kings;\nLook on my Works, ye Mighty, and despair!\nNothing beside remains. Round the decay\nOf that colossal Wreck, boundless and bare')
+print('The lone and level sands stretch far away.”')
